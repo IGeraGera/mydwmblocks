e20a7922426a353ea2b1cecadad1ea3f2a760e23
IGeraGera <gerasimoi@ece.auth.gr>
2020-12-26 22:26:38 +0200

    Added Basic Functionality

--- blocks.def.h
+++ blocks.def.h
@@ -5,8 +5,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", 	SHSCRIPT("memory.sh"),		5,		2},
-	{"", 	SHSCRIPT("keyboard.sh"),	1,		1},
+	{"", 	SHSCRIPT("battery.sh"),		5,		6},
+	{"", 	SHSCRIPT("weather.sh"),		1800,		5},
+	{"", 	SHSCRIPT("memory.sh"),		5,		4},
+	{"",	SHSCRIPT("volume.sh"),		0,		2},
+	{"", 	SHSCRIPT("keyboard.sh"),	0,		1},
 	{"", 	SHSCRIPT("time.sh"),		30,		3},
 };
 
