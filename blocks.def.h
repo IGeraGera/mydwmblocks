ae5857642d28b933172d45807329a6cf3b62d00a
IGeraGera <gerasimoi@ece.auth.gr>
2021-10-23 18:11:48 +0300

    Changed configuration scripts location

--- blocks.def.h
+++ blocks.def.h
@@ -1,6 +1,6 @@
 
 # define SHSCRIPT(name) \
-	("$HOME/.dwmblocks/"name)
+	("$HOME/.dwm/dwmblocks/"name)
 
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
